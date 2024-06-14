62642daf31b54e68c76e94da196660b9269ba66e
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2024-06-14 21:38:18 +0200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Get the current working directory
+const currentWorkingDirectory = new URL(".", import.meta.url).pathname;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": `${currentWorkingDirectory}/src`, // Set the base directory for your project
+      "@common": `${currentWorkingDirectory}/src/components/common`,
+      "@features": `${currentWorkingDirectory}/src/components/features`,
+      "@data": `${currentWorkingDirectory}/src/data`,
+      "@styles": `${currentWorkingDirectory}/src/styles`,
+    },
+  },
+});
